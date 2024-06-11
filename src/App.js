e39d90c8160b850ddbd0dcba1ba0e56d0a7c198b
twanng <nguyenthanhtwan29112002@gmail.com>
2024-06-11 17:07:57 +0700

    News page #1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Home from "./pages/Home/Home.js";
 import './App.css';
 import React from "react";
-import Contact from "./pages/contact/Contact";
-import Quote from "./pages/quote/Quote";
+
+import News from "./pages/news/News";
+
 
 function App() {
   return (
       <div className="App">
-        <Quote/>
+        <News/>
       </div>
   );
 }
--- src/pages/news/News.js
+++ src/pages/news/News.js
@@ -1,17 +1,17 @@
 // src/pages/Contact.js
 import React from 'react';
 import {Footer} from '../../components/footer/Footer';
-import QuoteContent from "../../components/Content/QuoteContent/QuoteContent";
 import Header from "../../components/header2/Header";
+import NewsContent from "../../components/Content/NewsContent/NewsContent";
 
-function Quote() {
+function News() {
   return (
       <div>
         <Header/>
-        <QuoteContent/>
+        <NewsContent/>
         <Footer/>
       </div>
   );
 }
 
-export default Quote;
+export default News;
