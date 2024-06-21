e6f7a872da49188dabfdc819268a1937b457a661
twanng <nguyenthanhtwan29112002@gmail.com>
2024-06-21 13:58:55 +0700

    Add Common Component( Pages, BannerWrapper)... and 3 pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/Home';
-
+import Introduction from './pages/Introduction/Introduction';
 const router = createBrowserRouter([
   {
     path : '/',
@@ -14,13 +14,15 @@ const router = createBrowserRouter([
     path : '/trang-chu',
     element : <Home />
   },
-  
+  {
+    path : '/gioi-thieu',
+    element : <Introduction />
+  }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode >
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+    <React.StrictMode >
+      <RouterProvider router={router}/>
+    </React.StrictMode>
 );
-
